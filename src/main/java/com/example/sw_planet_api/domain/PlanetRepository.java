dc351ed474d813aa22f30bbffdf07131e5d41b84
Lucas Oliveira Silvério <oliveiratricolorfc@hotmail.com>
2026-01-14 22:28:15 -0300

    Projeto Criado

---
+++ src/main/java/com/example/sw_planet_api/domain/PlanetRepository.java
@@ -0,0 +1,8 @@
+package com.example.sw_planet_api.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlanetRepository extends CrudRepository<Planet, Long> {
+
+
+}
